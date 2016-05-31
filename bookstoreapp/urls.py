c7684e16f163120aebbd01f66b44a394e75fa6ca
nmlng <nuno.m.guerreiro@gmail.com>
2016-05-31 19:59:31 +0100

    urls fix

--- bookstoreapp/urls.py
+++ bookstoreapp/urls.py
@@ -5,10 +5,10 @@ from . import views
 app_name = 'bookstoreapp'
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
-    url(r'^createbook$', 'bookstoreapp.views.bookcreate', name='bookcreate'),
-    url(r'^createeditor$', 'bookstoreapp.views.editorcreate', name='editorcreate'),
-    url(r'^createauthor$', 'bookstoreapp.views.authorcreate', name='authorcreate'),
-    url(r'^createcategory$', 'bookstoreapp.views.categorycreate', name='authorcreate'),
+    url(r'^createbook$', views.bookcreate, name='bookcreate'),
+    url(r'^createeditor$', views.editorcreate, name='editorcreate'),
+    url(r'^createauthor$', views.authorcreate, name='authorcreate'),
+    url(r'^createcategory$', views.categorycreate, name='authorcreate'),
     url(r'^(?P<category_id>[0-9]+)/books/$', views.category_view, name='category_view'),
 #    url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
 #    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
